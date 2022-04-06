9f6e05d952dbabbb9bad1ae35ee1a5ba1ef4de51
teregonzalez <teregonzalezpro@gmail.com>
2022-04-06 17:20:57 -0400

    Docs agregado

---
+++ docs/precache-manifest.30e11cb759d120beec98ef5d64e57fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fc3390cce21a9f91660a57dc016571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef1820534473cdb65461",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1820534473cdb65461",
+    "url": "/static/js/main.35ae0d76.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
